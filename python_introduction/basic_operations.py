d8dbdf06934b22d297869b91c8708c7f1c4ec6f3
Mickey <michaelzewdu1996@gmail.com>
2025-11-08 21:29:10 +0300

    organized the file structure

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+print("Addition of", number1, "and", number2, "is", sum_result)
+difference_result = number2 - number1
+print("Subtraction of", number1, "and", number2, "is", difference_result)            
+product_result = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", product_result)
