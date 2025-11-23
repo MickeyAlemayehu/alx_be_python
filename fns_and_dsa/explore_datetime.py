885b3afced48af8cd7ab1ce84a093175ca5aa625
Mickey <michaelzewdu1996@gmail.com>
2025-11-23 20:16:13 +0300

    datetime task done

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,6 +12,7 @@ def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: ")) 
     current_date = display_current_datetime()
     future_date = current_date + timedelta(days=number_of_days) 
-    print ("Future date: ", future_date.date())
+    future_date = future_date.strftime("%Y-%m-%d")
+    print ("Future date: ", future_date.date)
 
 calculate_future_date()
