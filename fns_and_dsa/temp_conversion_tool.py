4e1ead0b1c8c619874e4e42bbb3416372eaebb2b
Mickey <michaelzewdu1996@gmail.com>
2025-11-23 20:55:03 +0300

    temp conversion task done

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,10 +2,10 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(tmp):
-        temp = (tmp - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-        return temp
+    temp = (tmp - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temp
 def convert_to_fahrenheit(tmp):
-    temp = CELSIUS_TO_FAHRENHEIT_FACTOR * 32 + tmp
+    temp = CELSIUS_TO_FAHRENHEIT_FACTOR + 32 - tmp
     return temp
 def main():
     try:
