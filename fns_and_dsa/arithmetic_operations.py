e5b77b04dcdb25165d5126ae48bf23ca77056e5b
Mickey <michaelzewdu1996@gmail.com>
2025-11-23 19:02:53 +0300

    artimertic operation task done

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply" :
             return num1 * num2
     elif operation == "divide" :
-            if num2 != 0:   
-                return  num1 / num2   
-            else: 
+            if num2 == 0:   
                 return print("Division by zero is not defined")
-
-
+            else: 
+                return  num1 / num2 
