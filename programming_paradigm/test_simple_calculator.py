df3b03ef3924457c3c84ec21ff9271273289241e
Mickey <michaelzewdu1996@gmail.com>
2025-11-29 23:16:13 +0300

    unit test task done

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,10 +6,10 @@ class TestSimplecalc(unittest.TestCase):
         self.calc = SimpleCalculator()
     def test_addition(self):
         self.assertEqual(self.calc.add(5, 3), 8)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6) 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(7, 6), 42)
-    def test_divide(self): 
+    def test_division(self): 
         self.assertEqual(self.calc.divide(20, 4), 5)
         self.assertIsNone(self.calc.divide(10, 0))    
